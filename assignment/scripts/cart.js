9430f592165f856814a6925dded49660129014b1
Tina Tram <tinatram07@gmail.com>
2022-10-21 20:06:02 -0500

    added isFull(), updated addItem() with isFull()

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,10 +2,15 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 let basket = [];
+const maxItems = 5;
 
 function addItem(item) {
-    basket.push(item);
-    return true;
+    if (isFull() === false) {
+        basket.push(item);
+        return true;
+    } else if (isFull() === true) {
+        return false;
+    }
 }
 
 function listItems(){
@@ -21,13 +26,16 @@ function empty() {
 addItem('apples');
 addItem('lemons');
 addItem('oranges');
+addItem('grapes');
+
 
 console.log(`Basket is ${basket}`);
-console.log('Adding tomatoes - should say true:', addItem('tomatoes'));
+console.log('Adding bananas - should say true:', addItem('bananas'));
+console.log('Adding tomatoes - should say false:', addItem('tomatoes'));
 console.log(`Basket is now ${basket}`);
-console.log(listItems());
+listItems();
 empty();
-console.log(`The basket should now be empty ${basket}`);
+console.log(`Emptying basket...The basket should now be empty ${basket}`);
 console.log(basket);
 
 addItem('juice');
@@ -39,3 +47,13 @@ console.log(`Basket is now ${basket}`);
 console.log(basket);
 
 //Stretch Goals
+function isFull() {
+    if (basket.length < maxItems) {
+        return false;
+    } else if (basket.length >= maxItems) {
+        return true;
+    }
+}
+console.log('Is basket full:',isFull());
+console.log('Adding smoothie - should say true:', addItem('smoothie'));
+console.log('Is basket full:',isFull());
