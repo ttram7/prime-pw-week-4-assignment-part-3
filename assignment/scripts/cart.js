85f9ccf61dbad8af07f6b45d19737877a2694311
Tina Tram <tinatram07@gmail.com>
2022-10-23 20:44:27 -0500

    tested all functions

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -13,40 +13,17 @@ function addItem(item) {
     }
 }
 
-function listItems(){
+function listItems() {
     for (let i = 0; i < basket.length; i++) {
-        console.log(`Ingredient ${[i+1]}:`,basket[i]);
+        console.log(`Ingredient ${[i+1]}: ${basket[i]}`);
     }
 }
 
 function empty() {
     basket = [];
+    return basket;
 }
 
-addItem('apples');
-addItem('lemons');
-addItem('oranges');
-addItem('grapes');
-
-
-console.log(`Basket is ${basket}`);
-console.log('Adding bananas - should say true:', addItem('bananas'));
-console.log('Adding tomatoes - should say false:', addItem('tomatoes'));
-console.log(`Basket is now ${basket}`);
-listItems();
-empty();
-console.log(`Emptying basket...The basket should now be empty ${basket}`);
-console.log(basket);
-
-addItem('juice');
-addItem('water');
-addItem('milk');
-console.log(`Basket is ${basket}`);
-console.log('Adding soda - should say true:', addItem('soda'));
-console.log(`Basket is now ${basket}`);
-console.log(basket);
-
-//Stretch Goals
 function isFull() {
     if (basket.length < maxItems) {
         return false;
@@ -54,9 +31,6 @@ function isFull() {
         return true;
     }
 }
-console.log('Is basket full:',isFull());
-console.log('Adding smoothie - should say true:', addItem('smoothie'));
-console.log('Is basket full:',isFull());
 
 function removeItem(item) {
     if (basket.includes(item) === true) {
@@ -65,10 +39,38 @@ function removeItem(item) {
         return deletedItem;
     } else {
         return null;
-        //return `Item not in basket. Cannot be removed: ${item}`;
     }
 }
 
-console.log(basket);
-console.log(removeItem('soup'));
-console.log(basket);
+console.log('-----Testing addItem-----')
+console.log(`Basket is ${basket}`);
+console.log('Adding bananas - should say true:', addItem('bananas'));
+console.log('Adding tomatoes - should say true:', addItem('tomatoes'));
+console.log('Adding oranges - should say true:', addItem('oranges'));
+console.log('Adding apples - should say true:', addItem('apples'));
+console.log('Adding lemons - should say true:', addItem('lemons'));
+console.log('Adding grapes - should say false:', addItem('grapes'));
+console.log(`Basket is now ${basket}`);
+
+console.log('-----Testing listItems-----')
+listItems();
+
+console.log('-----Testing empty-----')
+console.log('Emptying basket...The basket should now be empty', empty());
+console.log(`Basket is now ${basket}`);
+
+console.log('-----Testing isFull-----')
+console.log('Adding juice - should say true:',addItem('juice'));
+console.log('Adding water - should say true:',addItem('water'));
+console.log('Adding milk - should say true:',addItem('milk'));
+console.log('Adding soda - should say true:', addItem('soda'));
+console.log(`Basket is now ${basket}`);
+console.log('Is basket full:',isFull());
+console.log('Adding smoothie - should say true:', addItem('smoothie'));
+console.log(`Basket is now ${basket}`);
+console.log('Is basket full:',isFull());
+
+console.log('-----Testing removeItem-----')
+console.log(`Removing item: ${removeItem('water')}`);
+console.log(`Basket is now ${basket}`);
+console.log(`Removing item: ${removeItem('soup')}`);
